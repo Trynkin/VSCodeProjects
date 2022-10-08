2945dda369fb4e2839f58ca102ceeea414c85f99
maksim <m.trynkin@g.nsu.ru>
2022-10-08 17:38:05 +0700

    Initial Commit

---
+++ C++/Password Manager/psw.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (0 == argc)
+    {
+    }
+    else
+    {
+    while(-1 != getopt(argc, argv,"a:g");
+    cout << "Какой пароль нужен?" << endl;
+    }
+}
