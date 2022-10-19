b8ea59b9a8374138bd820c2a86665bb8e443b90a
maksim <m.trynkin@g.nsu.ru>
2022-10-20 05:19:52 +0700

    getopt_long

--- C++/Password Manager/psw.cpp
+++ C++/Password Manager/psw.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <unistd.h>
+#include <getopt.h>
+#include <boost/json.hpp>
 
 using namespace std;
+using namespace boost::json;
 
 int main(int argc, char *argv[])
 {
+    int c;
+    int digit_optind = 0;
     if (0 == argc)
     {
     }
     else
     {
-        string search;
+        std::string search;
         char opt;
-    while((opt = getopt(argc, argv,"a:s") != -1)){
-        switch (opt)
-        {
-        case 'a':
-            cin >> search;
-            break;
+    while(1) {
+        static struct option long_option[] = {
+            {"add", required_argument, NULL, 'a'},
+            {"get", required_argument, NULL, 'g'},
+            {0,     0,                 0,     0 }
+        };
         
-        default:
-            break;
-        }
+    }
     }
     std::cout << "Какой пароль нужен?" << endl;
-    }
 }
